a538f85bd987b7985edd50017ec44304fe51a232
Keshriraj7870 <adityabkrajs@gmail.com>
2021-07-24 08:19:00 +0530

    quiz app completed

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ const quizData=[
     },
     {
         question:'How old Hagreed was ?',
-        a:'10',
-        b:'100',
+        a:'30',
+        b:'440',
         c:'130',
         d:'forever',
         correct:'b'
     },{
         question:'How old Abafrod was ?',
         a_1:'10',
-        b:'100',
+        b:'1070',
         c:'130',
         d:'forever',
-        correct:'c'
+        correct:'d'
     }
 ];
 
@@ -36,34 +36,87 @@ const option4 = document.getElementById('#option4');
 
 const submitBtn =document.getElementById('#submit')
 
+const quiz =document.getElementById('#quiz')
+
 let currentQuestion =0;
+let answer= undefined;
+let score =0;
 
 loadQuestion();
 
 function loadQuestion(){
-    
+    deSelectElement();
     const currentQuizData=quizData[currentQuestion]
     
-    console.log(currentQuizData.question);
     $('#question').html(currentQuizData.question);
     $('#option1').html(currentQuizData.a);
     $('#option2').html(currentQuizData.b);
     $('#option3').html(currentQuizData.c);
     $('#option4').html(currentQuizData.d);
 
-    currentQuestion++;
+    // currentQuestion++;
 }
 
 $('#submit').on('click',function(){
-    currentQuestion++;
+    //check to see the answer
+    const answer=   getSelected();
 
-    if(currentQuestion< currentQuizData.length){
+ if(answer)
+ {
 
+    if(answer === quizData[currentQuestion].correct){
+        score++;
+        
+    }
+
+     currentQuestion++;
+     if(currentQuestion< quizData.length){
         loadQuestion();
+        // score++;
     }
     else{
-        // show Result
-        alert("Finish");
-    }
+        
 
-})
+        $('#quiz').html(`<h2>You answered correctly at ${score} out of 
+        ${quizData.length} 
+        </h2>
+        <div class="refresh-button"><button onClick="location.reload();">Refresh</button></div>
+        
+        `)
+       
+    }
+         
+     }
+     else{
+         alert("Please select the answer")
+     }
+     
+     
+        
+
+});
+
+function getSelected(){
+
+    let answer =undefined;
+    const answerEls =document.querySelectorAll('.answer');
+    answerEls.forEach((answerEls)=>{
+        if(answerEls.checked){
+            answer=answerEls.id;
+
+        }
+        
+    });
+ return answer;
+
+    
+}
+
+function deSelectElement(){
+    const answerEls =document.querySelectorAll('.answer');
+    answerEls.forEach((answerEls)=>{
+        answerEls.checked=false;
+        
+    });
+
+}
